46ff10546464d67f87f96e58a0e80cd3d4cfac53
Rajan Kumar <developer.rajan2@gmail.com>
2023-03-31 19:35:49 +0530

    updated namespace

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,11 +6,11 @@
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'reference' => NULL,
-        'name' => 'julitroalves/twitter-video-downloader',
+        'name' => 'rajandev/twitter-video-downloader',
         'dev' => true,
     ),
     'versions' => array(
-        'julitroalves/twitter-video-downloader' => array(
+        'rajandev/twitter-video-downloader' => array(
             'pretty_version' => '1.0.0+no-version-set',
             'version' => '1.0.0.0',
             'type' => 'library',
